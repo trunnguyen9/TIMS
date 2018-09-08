0be89852d715157c3de03cb816217244326f259e
darrellmiller <darrellrhodesmiller@gmail.com>
2018-09-08 13:29:21 -0500

    Initial File creation and description of purpose

---
+++ Backend_Processor/DownloadAgent/IoC_Modules/__init__.py
@@ -0,0 +1,6 @@
+# __init__.py
+# IOC_Modules
+# This module contains parsers for each of the open source data sources
+# each file is a different open source database of *Indicators of Compromise*/Internet Threats
+
+from IoC_PhishTank import IocPhishTank
