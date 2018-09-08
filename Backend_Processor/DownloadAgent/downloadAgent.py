1b2b959b4cfe23d5132001cb27c7ee478d81c901
darrellmiller <darrellrhodesmiller@gmail.com>
2018-09-08 09:05:32 -0500

    Initial File creation and description of purpose

---
+++ Backend_Processor/DownloadAgent/downloadAgent.py
@@ -0,0 +1,32 @@
+# --====================================================--
+# Threat Information Management System (T.I.M.S.)
+# Download Agent
+# Group 2 - Fall 2018
+# Darrell Miller, Doug Peck, Raymond Schmalzl, Trung Nguyen
+#
+# --====================================================--
+#
+# This application will pull IOC (indicators of compromise) threat information from multiple sources then
+# consolidate those sources into a database. Another application will then pull that information from the database
+# and turn it into a form a firewall or IDS system can use.
+# list of open threat feeds:
+# - http://www.covert.io/threat-intelligence/
+# - https://github.com/mlsecproject/combine/wiki/Threat-Intelligence-Feeds-Gathered-by-Combine
+# This is the "hunter/gatherer" of this system. It will go through multiple sites and pull the information
+# --====================================================--
+#
+#
+# --====================================================--
+# @TODO Read Configuration File made from GUI Front End
+# @TODO Connect to sqllite database
+# @TODO Connect to threat databases (TDs)
+# @TODO Normalize information from TDs and put them in the same format
+# @TODO Enrich threat data - add more information to what comes from the TDs
+# @TODO          - IP addresses need DNS info, GPS info
+# @TODO          - FQDN, DNS need IP address resolved, and GPS info
+# @TODO          - email address : no idea how we can enrich
+# @TODO          - MD5/SHA1 hash of malware/virus: no idea how we can enrich
+# @TODO   * Don't know if we can do this as we ingest the data, or if it will take too long. Might be better
+# @TODO   * to ingest quickly then have another agent that goes back and enriches the data, as another process
+
+# --====================================================--
