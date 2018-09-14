0f16dd40b9bde933dfe55edb4bd253f9cb7a9cba
trunnguyen9 <trung_texas@hotmail.com>
2018-09-14 17:58:31 -0500

    - Add Init Setup Environment with Angular Framework - A Login Page

---
+++ FrontEnd_GUI/Tims/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ConfigComponent } from './config/config.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ConfigComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
