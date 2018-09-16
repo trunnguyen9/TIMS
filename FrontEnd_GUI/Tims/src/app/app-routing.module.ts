a900c1e4c46fb947f77afe888489cc3e40bf5338
Trung Nguyen <trung_texas@hotmail.com>
2018-09-16 13:07:39 -0500

    Add config page

--- FrontEnd_GUI/Tims/src/app/app-routing.module.ts
+++ FrontEnd_GUI/Tims/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { ConfigComponent } from './config/config.component';
 
 const routes: Routes = [
     {
@@ -16,6 +17,10 @@ const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent
+    },
+    {
+        path: 'configuration',
+        component: ConfigComponent
     }
 ];
 
