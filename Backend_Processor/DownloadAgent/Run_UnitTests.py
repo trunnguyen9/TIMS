f5f4bb2afca33eb800a806b0827f4c2fe2801d80
Douglas Scott Peck <Scott@webhead.local>
2018-09-22 12:55:39 -0400

    Updated unit test run script to include export module tests and new IoC unit tests

--- Backend_Processor/DownloadAgent/Run_UnitTests.py
+++ Backend_Processor/DownloadAgent/Run_UnitTests.py
@@ -9,22 +9,42 @@
 # Basic Run Script to Execute unite tests for defined test cases
 
 import unittest
-from DataStore_Modules import *
+# from DataStore_Modules import *
 from IoC_Modules import *
 from UnitTest import *
 
 # Create a UnitTest Loader to discover test names/TestCase
 loader = unittest.TestLoader()
+
+print("===================== BEGIN IoC UNIT TESTS =====================")
 # Set up a suite to house tests
 suite  = unittest.TestSuite()
 
 # Extract Tests from TestCases
 suite.addTests(loader.loadTestsFromTestCase(Test_IoC_EmergingThreats))
 suite.addTests(loader.loadTestsFromTestCase(Test_IoC_PhishTank))
+suite.addTests(loader.loadTestsFromTestCase(Test_IoC_AlienVault))
+suite.addTests(loader.loadTestsFromTestCase(Test_IoC_Feodotracker))
+suite.addTests(loader.loadTestsFromTestCase(Test_IoC_CSIRTG))
 
 # Run All Test Cases
 runner = unittest.TextTestRunner(verbosity=3)
 result = runner.run(suite)
+print("====================== END IoC UNIT TESTS ======================")
+print("")
+print("")
+print("==================== BEGIN EXPORT UNIT TESTS ====================")
+# Set up a suite to house tests
+suite  = unittest.TestSuite()
+
+# Extract Tests from TestCases
+suite.addTests(loader.loadTestsFromTestCase(Test_ExportModule))
+
+# Run All Test Cases
+runner = unittest.TextTestRunner(verbosity=2)
+result = runner.run(suite)
+print("===================== END EXPORT UNIT TESTS =====================")
+
 
 
 
