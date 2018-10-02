5c7cfc200aefe5f6d42858c579b8433d0091a95b
Douglas Scott Peck <Scott@webhead.local>
2018-10-01 23:54:34 -0400

    Updated Run_UnitTests.py script to be complient with newly added IoC modules

--- Backend_Processor/DownloadAgent/Run_UnitTests.py
+++ Backend_Processor/DownloadAgent/Run_UnitTests.py
@@ -11,21 +11,28 @@
 import unittest
 # from DataStore_Modules import *
 from IoC_Modules import *
-from UnitTest import *
+from UnitTest import Test_IoC, Test_IoC_All, Test_ExportModule
 
 # Create a UnitTest Loader to discover test names/TestCase
 loader = unittest.TestLoader()
-
+print("")
 print("===================== BEGIN IoC UNIT TESTS =====================")
+print("")
 # Set up a suite to house tests
 suite  = unittest.TestSuite()
 
 # Extract Tests from TestCases
-suite.addTests(loader.loadTestsFromTestCase(Test_IoC_EmergingThreats))
-suite.addTests(loader.loadTestsFromTestCase(Test_IoC_PhishTank))
-suite.addTests(loader.loadTestsFromTestCase(Test_IoC_AlienVault))
-suite.addTests(loader.loadTestsFromTestCase(Test_IoC_Feodotracker))
-suite.addTests(loader.loadTestsFromTestCase(Test_IoC_CSIRTG))
+suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_AlienVault))
+suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_EmergingThreats))
+suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_Feodotracker))
+suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_CSIRTG))
+# suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_NetLabs360))
+suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_NoThink))
+suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_OpenPhish))
+suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_PhishTank))
+suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_SANsEDU))
+suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_SpamHaus))
+suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_Zeus))
 
 # Run All Test Cases
 runner = unittest.TextTestRunner(verbosity=3)
@@ -33,7 +40,7 @@ result = runner.run(suite)
 print("====================== END IoC UNIT TESTS ======================")
 print("")
 print("")
-print("==================== BEGIN EXPORT UNIT TESTS ====================")
+print("================ BEGIN EXPORT MODULE UNIT TESTS ================")
 # Set up a suite to house tests
 suite  = unittest.TestSuite()
 
@@ -43,7 +50,7 @@ suite.addTests(loader.loadTestsFromTestCase(Test_ExportModule))
 # Run All Test Cases
 runner = unittest.TextTestRunner(verbosity=3)
 result = runner.run(suite)
-print("===================== END EXPORT UNIT TESTS =====================")
+print("================= END EXPORT MODULE UNIT TESTS =================")
 
 
 
