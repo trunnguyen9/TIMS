5862663ae1aad3b164b71765cb021680d0d4d284
Trung Nguyen <trung_texas@hotmail.com>
2018-11-18 14:38:20 -0600

    download feature

--- FrontEnd_GUI/Tims/src/app/Services/config.service.ts
+++ FrontEnd_GUI/Tims/src/app/Services/config.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { Config } from '../Components/config/config.component';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
