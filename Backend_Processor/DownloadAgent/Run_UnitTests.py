f8b763f86b6635fcf8bf9fe13e06890a99cc5d3b
DougScottPeck <dougscottpeck@gmail.com>
2018-12-01 22:28:25 -0500

    Updated Unit Test Run Script for Server Testing

--- Backend_Processor/DownloadAgent/Run_UnitTests.py
+++ Backend_Processor/DownloadAgent/Run_UnitTests.py
@@ -12,47 +12,67 @@ import unittest
 import nose
 # from DataStore_Modules import *
 from modules import *
-from UnitTest import Test_ExportModule, Test_IoC,Test_IoC_All
+from UnitTest import *
 
 # Create a UnitTest Loader to discover test names/TestCase
 loader = unittest.TestLoader()
-print("")
-print("===================== BEGIN IoC UNIT TESTS =====================")
-print("")
-# Set up a suite to house tests
-suite  = unittest.TestSuite()
-
-# Extract Tests from TestCases
-# suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_AlienVault))
-# suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_EmergingThreats))
-# suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_Feodotracker))
-# suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_CSIRTG))
-# suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_NetLabs360))
-# suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_NoThink))
-# suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_OpenPhish))
-# suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_PhishTank))
-# suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_SANsEDU))
-# suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_SpamHaus))
-suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_Zeus))
-
-# Run All Test Cases
-runner = unittest.TextTestRunner(verbosity=3)
-result = runner.run(suite)
-print("====================== END IoC UNIT TESTS ======================")
-print("")
 print("")
 print("================ BEGIN EXPORT MODULE UNIT TESTS ================")
 # Set up a suite to house tests
 suite  = unittest.TestSuite()
 
 # Extract Tests from TestCases
-# suite.addTests(loader.loadTestsFromTestCase(Test_ExportModule))
+suite.addTests(loader.loadTestsFromTestCase(Test_ExportModule))
 
 # Run All Test Cases
-runner = unittest.TextTestRunner(verbosity=3)
+runner = unittest.TextTestRunner(buffer=True,verbosity=2)
 result = runner.run(suite)
 print("================= END EXPORT MODULE UNIT TESTS =================")
+print("")
+print("================ BEGIN ENRICHMENT MODULE UNIT TESTS ================")
+# Set up a suite to house tests
+suite  = unittest.TestSuite()
 
+# Extract Tests from TestCases
+suite.addTests(loader.loadTestsFromTestCase(Test_DataEnricher))
+suite.addTests(loader.loadTestsFromTestCase(Test_HostIP))
+suite.addTests(loader.loadTestsFromTestCase(Test_LocateGeoIP))
 
+# Run All Test Cases
+runner = unittest.TextTestRunner(buffer=True,verbosity=2)
+result = runner.run(suite)
+print("================= END ENRICHMENT MODULE UNIT TESTS =================")
+print("")
+print("================ BEGIN USERSTORE MODULE UNIT TESTS ================")
+# Set up a suite to house tests
+suite  = unittest.TestSuite()
 
+# Extract Tests from TestCases
+suite.addTests(loader.loadTestsFromTestCase(Test_UserStore))
+
+# Run All Test Cases
+runner = unittest.TextTestRunner(buffer=True,verbosity=2)
+result = runner.run(suite)
+print("================= END USERSTORE MODULE UNIT TESTS =================")
+print("")
+print("===================== BEGIN IoC UNIT TESTS =====================")
+# Set up a suite to house tests
+suite  = unittest.TestSuite()
+
+# Extract Tests from TestCases
+suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_AlienVault))
+suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_EmergingThreats))
+suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_Feodotracker))
+suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_NetLabs360))
+suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_NoThink))
+suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_OpenPhish))
+suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_PhishTank))
+suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_SANsEDU))
+suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_SpamHaus))
+suite.addTests(loader.loadTestsFromTestCase(Test_IoC_All.Test_IoC_Zeus))			
+
+# Run All Test Cases
+runner = unittest.TextTestRunner(buffer=True,verbosity=2)
+result = runner.run(suite)
+print("====================== END IoC UNIT TESTS ======================")
 
